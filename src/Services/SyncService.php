d0931cb88043f68f750c7eaa3abfe9604c09420b
Sharifur Rahman <rsharifur824@gmail.com>
2024-08-26 21:11:23 +0600

    fix: remove unused code

--- src/Services/SyncService.php
+++ src/Services/SyncService.php
@@ -5,28 +5,6 @@ use Xgenious\CloudflareR2Sync\Api\CloudflareR2Api;
 
 class SyncService {
 
-//    public function upload_to_cloudflare_r2($metadata, $attachment_id) {
-//        if (!get_option('cloudflare_r2_enabled')) {
-//            return $metadata;
-//        }
-//
-//        $file = get_attached_file($attachment_id);
-//        $cloudflare_r2_api = new CloudflareR2Api();
-//        $result = $cloudflare_r2_api->upload_file($file);
-//
-//        if ($result) {
-//            $this->log_sync_status($attachment_id, 'success', 'File uploaded to Cloudflare R2');
-//        } else {
-//            $this->log_sync_status($attachment_id, 'error', 'Failed to upload file to Cloudflare R2');
-//        }
-//
-//        return $metadata;
-//    }
-
-/*
-
-*/
-
     public function log_sync_status($file_id, $status, $message = '') {
         global $wpdb;
         $table_name = $wpdb->prefix . 'xg_cloudflare_r2_sync_log';
